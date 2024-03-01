2536dd432d2b34040c70d1337363ce6f04632e61
EasyApps <hey.easyapps@gmail.com>
2024-03-01 18:15:46 +0530

    Remove irrelevant strings

--- commons/src/main/kotlin/com/eadevs/commons/compose/screens/FAQScreen.kt
+++ commons/src/main/kotlin/com/eadevs/commons/compose/screens/FAQScreen.kt
@@ -85,7 +85,6 @@ private fun FAQScreenPreview() {
                 FAQItem(R.string.faq_1_title_commons, R.string.faq_1_text_commons),
                 FAQItem(R.string.faq_1_title_commons, R.string.faq_1_text_commons),
                 FAQItem(R.string.faq_4_title_commons, R.string.faq_4_text_commons),
-                FAQItem(R.string.faq_2_title_commons, R.string.faq_2_text_commons),
                 FAQItem(R.string.faq_6_title_commons, R.string.faq_6_text_commons)
             ).toImmutableList()
         )
